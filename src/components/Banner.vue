484c93cf1d926ded28b49028496d68fc7d3bfbf6
Neeraj Anand <56186425+neeraj-2@users.noreply.github.com>
2020-09-22 19:34:53 +0530

    Updation for mobile view

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -3,27 +3,42 @@
     <div
       class="padding-small"
       style="background: #e6f6eb;"
-      v-bind:class="{'change':task.id}">
+      v-bind:class="{ change: task.id }"
+    >
       <div class="columns">
         <div class="column is-8">
           <p class="margin-top-normal">
-            If you donate $5 today, and everyone else does the same,
-            we can keep growing our database of freely reusable content. For you. For all of us.
+            If you donate $5 today, and everyone else does the same, we can keep
+            growing our database of freely reusable content. For you. For all of
+            us.
           </p>
         </div>
+
         <div class="column">
-          <button type="submit" class="button is-success is-small" v-on:click="redirect">
-            <i class="icon cc-letterheart-filled margin-right-small is-size-5 padding-top-smaller" />Yes, I’ll donate now
+          <button
+            type="submit"
+            class="button is-success is-small position"
+            v-on:click="redirect"
+          >
+            <i
+              class="icon cc-letterheart-filled margin-right-small is-size-5 padding-top-smaller"
+            />Yes, I’ll donate now
+          </button>
+        </div>
+
+        <div class="column">
+          <button
+            class="button is-small position"
+            style="border: none; background: #e6f6eb; color: #008000;"
+            v-on:click="onclick"
+          >
+            Not today
           </button>
         </div>
         <div class="column">
-          <button
-            class="button is-small"
-            style=" border:none;background: #e6f6eb; color:#008000"
-            v-on:click="onclick">Not today</button>
-        </div>
-        <div class="column">
-          <button class="button is-text" v-on:click="onclick">x</button>
+          <button class="button is-text button_two" v-on:click="onclick">
+            x
+          </button>
         </div>
       </div>
     </div>
@@ -34,7 +49,7 @@ export default {
   name: 'Banner',
   methods: {
     onclick() {
-      this.task.id = true;
+      this.task.id = true
     },
     redirect() {
       window.location.href =
@@ -53,5 +68,15 @@ export default {
 <style scoped>
 .change {
   display: none;
+}
+@media only screen and (max-width: 767px) {
+  .button_two {
+    display: none;
+  }
+}
+@media only screen and (max-width: 767px) {
+  .position {
+    width: 100%;
+  }
 }
 </style>
